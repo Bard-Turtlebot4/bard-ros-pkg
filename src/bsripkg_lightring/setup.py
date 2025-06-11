e04a6c9cc5b34be233d38346bc301de14d03bc0a
Juan Mora <juanjuo37@gmail.com>
2025-06-11 15:05:52 -0400

    added first pkg

---
+++ src/bsripkg_lightring/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'bsripkg_lightring'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='juan',
+    maintainer_email='juanjuo37@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'bsri_node_lightring_test = bsripkg_lightring.bsri_node_lightring_test:main'
+        ],
+    },
+)
